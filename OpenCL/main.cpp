29f43c829227932f144497e98f363a96c5ca42b9
wangmingyong <my.want@icloud.com>
2017-09-19 03:16:14 +0800

    Initial Commit

---
+++ OpenCL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCL
+//
+//  Created by 王明勇 on 2017/9/19.
+//  Copyright © 2017年 王明勇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
