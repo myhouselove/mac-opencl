1a29e6aa2d955b3f6f1549e94b6a2b1ee05a5a71
wangmingyong <my.want@icloud.com>
2017-11-20 10:13:20 +0800

    kernel2

--- OpenCL/HelloWorld.cl
+++ OpenCL/HelloWorld.cl
@@ -3,23 +3,53 @@
 #pragma OPENCL EXTENSION cl_amd_printf : enable
 
 
-__kernel void hello_kernel(__global const int *a,
-                           __global const int *b,
-                           __global int *result_matrix,int  result_matrix_row,
-                           int  result_matrix_col,int  compute_size)
-{
-    int row = get_global_id(0);
-    int col = get_global_id(1);
+#define TS 16
 
-    printf("row =%d,col=%d\n",row,col);
-    int sum = 0;
-    for(int i=0;i<compute_size;i++)
-    {
-        sum += a[row*compute_size+i] * b[i*result_matrix_col+col];
+__kernel void hello_kernel(const __global int* A,
+                     const __global int* B,
+                     __global int* C, int M,  int N,  int K) {
+    
+    // Thread identifiers
+   // int size = get_local_size(1);
+    const int row = get_local_id(0); // Local row ID (max: TS)
+    const int col = get_local_id(1); // Local col ID (max: TS)
+    const int globalRow = TS*get_group_id(0) + row; // Row ID of C (0..M)
+    const int globalCol = TS*get_group_id(1) + col; // Col ID of C (0..N)
+
+    
+    //barrier(CLK_LOCAL_MEM_FENCE);
+    printf("here\n");
+    //printf("group row=%d,col=%d\n",get_group_id(0),get_group_id(1));
+    // Local memory to fit a tile of TS*TS elements of A and B
+    __local int Asub[TS][TS];
+    __local int Bsub[TS][TS];
+    
+    // Initialise the accumulation register
+    int acc = 0;
+    
+    // Loop over all tiles
+    const int numTiles = K/TS;
+    for (int t=0; t<numTiles; t++) {
+        
+        // Load one tile of A and B into local memory
+        const int tiledRow = TS*t + row;
+        const int tiledCol = TS*t + col;
+        Asub[col][row] = A[tiledCol*M + globalRow];
+        Bsub[col][row] = B[globalCol*K + tiledRow];
+        printf("Asub[%d][%d]=A[%d]=%d\t",col,row,tiledCol*M + globalRow,A[tiledCol*M + globalRow]);
+        // Synchronise to make sure the tile is loaded
+        barrier(CLK_LOCAL_MEM_FENCE);
+        //for(int j=0;j<)
+        // Perform the computation for a single tile
+        for (int k=0; k<TS; k++) {
+            acc += Asub[k][row] * Bsub[col][k];
+            //printf("acc[%d][%d]=%d\n",k,row,Asub[k][row]);
+        }
+       // printf("acc = %d\n",acc);
+        // Synchronise before loading the next tile
+        barrier(CLK_LOCAL_MEM_FENCE);
     }
-    printf("result_matrix[%d]=%d\n",row*result_matrix_col+col,sum);
-
-    result_matrix[row*result_matrix_col+col] = sum;
-
+    
+    // Store the final result in C
+    C[globalCol*M + globalRow] = acc;
 }
-
--- OpenCL/main.cpp
+++ OpenCL/main.cpp
@@ -27,10 +27,10 @@ using namespace std;
 
 //4*3---3*5
 
-const int midle = 10;
-const int heightA = 10;
+const int midle = 48;
+const int heightA = 48;
 
-const int widthB = 10;
+const int widthB = 48;
 //const int heightB = 3;
 
 //一、 选择OpenCL平台并创建一个上下文
@@ -82,10 +82,28 @@ cl_command_queue CreateCommandQueue(cl_context context, cl_device_id *device)
     
     // 为设备分配缓存空间
     devices = new cl_device_id[deviceBufferSize / sizeof(cl_device_id)];
+    printf("deviceBufferSize / sizeof(cl_device_id)=%ld\n",deviceBufferSize / sizeof(cl_device_id));
     errNum = clGetContextInfo(context, CL_CONTEXT_DEVICES, deviceBufferSize, devices, NULL);
     
+    
+    
+  
+//    size_t      valueSize;
+//    clGetDeviceInfo(devices[0], CL_DEVICE_NAME, 0, NULL, &valueSize);
+//    char* value = (char*) malloc(valueSize);
+//    clGetDeviceInfo(devices[0], CL_DEVICE_NAME, valueSize, value, NULL);
+//    printf("Device1 Name: %s\n", value);
+//    free(value);
+//    
+//    clGetDeviceInfo(devices[1], CL_DEVICE_NAME, 0, NULL, &valueSize);
+//    value = (char*) malloc(valueSize);
+//    clGetDeviceInfo(devices[1], CL_DEVICE_NAME, valueSize, value, NULL);
+//    printf("Device2 Name: %s\n", value);
+//    free(value);
+    
+    
     //选取可用设备中的第一个
-    commandQueue = clCreateCommandQueue(context, devices[0], 0, NULL);
+    commandQueue = clCreateCommandQueue(context, devices[1], 0, NULL);
     
     *device = devices[0];
     delete[] devices;
@@ -155,7 +173,82 @@ void Cleanup(cl_context context, cl_command_queue commandQueue,
     if (context != 0)
         clReleaseContext(context);
 }
+
+
+void checkError(cl_int error, int line) {
+    if (error != CL_SUCCESS) {
+        switch (error) {
+            case CL_DEVICE_NOT_FOUND:                 printf("-- Error at %d:  Device not found.\n", line); break;
+            case CL_DEVICE_NOT_AVAILABLE:             printf("-- Error at %d:  Device not available\n", line); break;
+            case CL_COMPILER_NOT_AVAILABLE:           printf("-- Error at %d:  Compiler not available\n", line); break;
+            case CL_MEM_OBJECT_ALLOCATION_FAILURE:    printf("-- Error at %d:  Memory object allocation failure\n", line); break;
+            case CL_OUT_OF_RESOURCES:                 printf("-- Error at %d:  Out of resources\n", line); break;
+            case CL_OUT_OF_HOST_MEMORY:               printf("-- Error at %d:  Out of host memory\n", line); break;
+            case CL_PROFILING_INFO_NOT_AVAILABLE:     printf("-- Error at %d:  Profiling information not available\n", line); break;
+            case CL_MEM_COPY_OVERLAP:                 printf("-- Error at %d:  Memory copy overlap\n", line); break;
+            case CL_IMAGE_FORMAT_MISMATCH:            printf("-- Error at %d:  Image format mismatch\n", line); break;
+            case CL_IMAGE_FORMAT_NOT_SUPPORTED:       printf("-- Error at %d:  Image format not supported\n", line); break;
+            case CL_BUILD_PROGRAM_FAILURE:            printf("-- Error at %d:  Program build failure\n", line); break;
+            case CL_MAP_FAILURE:                      printf("-- Error at %d:  Map failure\n", line); break;
+            case CL_INVALID_VALUE:                    printf("-- Error at %d:  Invalid value\n", line); break;
+            case CL_INVALID_DEVICE_TYPE:              printf("-- Error at %d:  Invalid device type\n", line); break;
+            case CL_INVALID_PLATFORM:                 printf("-- Error at %d:  Invalid platform\n", line); break;
+            case CL_INVALID_DEVICE:                   printf("-- Error at %d:  Invalid device\n", line); break;
+            case CL_INVALID_CONTEXT:                  printf("-- Error at %d:  Invalid context\n", line); break;
+            case CL_INVALID_QUEUE_PROPERTIES:         printf("-- Error at %d:  Invalid queue properties\n", line); break;
+            case CL_INVALID_COMMAND_QUEUE:            printf("-- Error at %d:  Invalid command queue\n", line); break;
+            case CL_INVALID_HOST_PTR:                 printf("-- Error at %d:  Invalid host pointer\n", line); break;
+            case CL_INVALID_MEM_OBJECT:               printf("-- Error at %d:  Invalid memory object\n", line); break;
+            case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:  printf("-- Error at %d:  Invalid image format descriptor\n", line); break;
+            case CL_INVALID_IMAGE_SIZE:               printf("-- Error at %d:  Invalid image size\n", line); break;
+            case CL_INVALID_SAMPLER:                  printf("-- Error at %d:  Invalid sampler\n", line); break;
+            case CL_INVALID_BINARY:                   printf("-- Error at %d:  Invalid binary\n", line); break;
+            case CL_INVALID_BUILD_OPTIONS:            printf("-- Error at %d:  Invalid build options\n", line); break;
+            case CL_INVALID_PROGRAM:                  printf("-- Error at %d:  Invalid program\n", line); break;
+            case CL_INVALID_PROGRAM_EXECUTABLE:       printf("-- Error at %d:  Invalid program executable\n", line); break;
+            case CL_INVALID_KERNEL_NAME:              printf("-- Error at %d:  Invalid kernel name\n", line); break;
+            case CL_INVALID_KERNEL_DEFINITION:        printf("-- Error at %d:  Invalid kernel definition\n", line); break;
+            case CL_INVALID_KERNEL:                   printf("-- Error at %d:  Invalid kernel\n", line); break;
+            case CL_INVALID_ARG_INDEX:                printf("-- Error at %d:  Invalid argument index\n", line); break;
+            case CL_INVALID_ARG_VALUE:                printf("-- Error at %d:  Invalid argument value\n", line); break;
+            case CL_INVALID_ARG_SIZE:                 printf("-- Error at %d:  Invalid argument size\n", line); break;
+            case CL_INVALID_KERNEL_ARGS:              printf("-- Error at %d:  Invalid kernel arguments\n", line); break;
+            case CL_INVALID_WORK_DIMENSION:           printf("-- Error at %d:  Invalid work dimensionsension\n", line); break;
+            case CL_INVALID_WORK_GROUP_SIZE:          printf("-- Error at %d:  Invalid work group size\n", line); break;
+            case CL_INVALID_WORK_ITEM_SIZE:           printf("-- Error at %d:  Invalid work item size\n", line); break;
+            case CL_INVALID_GLOBAL_OFFSET:            printf("-- Error at %d:  Invalid global offset\n", line); break;
+            case CL_INVALID_EVENT_WAIT_LIST:          printf("-- Error at %d:  Invalid event wait list\n", line); break;
+            case CL_INVALID_EVENT:                    printf("-- Error at %d:  Invalid event\n", line); break;
+            case CL_INVALID_OPERATION:                printf("-- Error at %d:  Invalid operation\n", line); break;
+            case CL_INVALID_GL_OBJECT:                printf("-- Error at %d:  Invalid OpenGL object\n", line); break;
+            case CL_INVALID_BUFFER_SIZE:              printf("-- Error at %d:  Invalid buffer size\n", line); break;
+            case CL_INVALID_MIP_LEVEL:                printf("-- Error at %d:  Invalid mip-map level\n", line); break;
+            case -1024:                               printf("-- Error at %d:  *clBLAS* Functionality is not implemented\n", line); break;
+            case -1023:                               printf("-- Error at %d:  *clBLAS* Library is not initialized yet\n", line); break;
+            case -1022:                               printf("-- Error at %d:  *clBLAS* Matrix A is not a valid memory object\n", line); break;
+            case -1021:                               printf("-- Error at %d:  *clBLAS* Matrix B is not a valid memory object\n", line); break;
+            case -1020:                               printf("-- Error at %d:  *clBLAS* Matrix C is not a valid memory object\n", line); break;
+            case -1019:                               printf("-- Error at %d:  *clBLAS* Vector X is not a valid memory object\n", line); break;
+            case -1018:                               printf("-- Error at %d:  *clBLAS* Vector Y is not a valid memory object\n", line); break;
+            case -1017:                               printf("-- Error at %d:  *clBLAS* An input dimension (M,N,K) is invalid\n", line); break;
+            case -1016:                               printf("-- Error at %d:  *clBLAS* Leading dimension A must not be less than the size of the first dimension\n", line); break;
+            case -1015:                               printf("-- Error at %d:  *clBLAS* Leading dimension B must not be less than the size of the second dimension\n", line); break;
+            case -1014:                               printf("-- Error at %d:  *clBLAS* Leading dimension C must not be less than the size of the third dimension\n", line); break;
+            case -1013:                               printf("-- Error at %d:  *clBLAS* The increment for a vector X must not be 0\n", line); break;
+            case -1012:                               printf("-- Error at %d:  *clBLAS* The increment for a vector Y must not be 0\n", line); break;
+            case -1011:                               printf("-- Error at %d:  *clBLAS* The memory object for Matrix A is too small\n", line); break;
+            case -1010:                               printf("-- Error at %d:  *clBLAS* The memory object for Matrix B is too small\n", line); break;
+            case -1009:                               printf("-- Error at %d:  *clBLAS* The memory object for Matrix C is too small\n", line); break;
+            case -1008:                               printf("-- Error at %d:  *clBLAS* The memory object for Vector X is too small\n", line); break;
+            case -1007:                               printf("-- Error at %d:  *clBLAS* The memory object for Vector Y is too small\n", line); break;
+            case -1001:                               printf("-- Error at %d:  Code -1001: no GPU available?\n", line); break;
+            default:                                  printf("-- Error at %d:  Unknown with code %d\n", line, error);
+        }
+        exit(1);
+    }
+}
 #define TIMES 1
+#define TS 16
 
 int main(int argc, char** argv)
 {
@@ -179,6 +272,18 @@ int main(int argc, char** argv)
     
     // 二、 创建设备并创建命令队列
     commandQueue = CreateCommandQueue(context, &device);
+    
+    
+    size_t      maxWorkItemPerGroup;
+    clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE,sizeof(maxWorkItemPerGroup), &maxWorkItemPerGroup, NULL);
+    printf("maxWorkItemPerGroup: %zd\n", maxWorkItemPerGroup);
+    
+    size_t      valueSize;
+    clGetDeviceInfo(device, CL_DEVICE_NAME, 0, NULL, &valueSize);
+    char* value = (char*) malloc(valueSize);
+    clGetDeviceInfo(device, CL_DEVICE_NAME, valueSize, value, NULL);
+    printf("Device Name: %s\n", value);
+    free(value);
     
     //创建和构建程序对象
     program = CreateProgram(context, device, filename);//"HelloWorld.cl");
@@ -248,12 +353,19 @@ int main(int argc, char** argv)
     size_t globalWorkSize[2];
     globalWorkSize[0]= heightA;
     globalWorkSize[1]=widthB;
-   // size_t localWorkSize[2] = { 1,1 };
-     t3 = clock();
+    size_t localWorkSize[2] ;
+    localWorkSize[0]= TS;
+    localWorkSize[1]= TS;
+    t3 = clock();
     for(int run=0;run<TIMES;run++){
         errNum = clEnqueueNDRangeKernel(commandQueue, kernel, 2, NULL,
-                                        globalWorkSize, NULL,
+                                        globalWorkSize, localWorkSize,
                                         0, NULL, NULL);
+        checkError(errNum,__LINE__);
+        if(errNum == CL_SUCCESS)
+            cout<<"enqueue success!"<<endl;
+        else
+            printf("errNum= %d\n",errNum);
     }
     
 
@@ -262,9 +374,9 @@ int main(int argc, char** argv)
     errNum = clEnqueueReadBuffer(commandQueue, memObjects[2], CL_TRUE,
                                  0, widthB*heightA * sizeof(int), result,
                                  0, NULL, NULL);
-//    for(int p=0;p<20;p++){
-//        cout<<"new ="<<result[p];
-//    }
+    for(int p=0;p<20;p++){
+        cout<<"new ="<<result[p];
+    }
     
 
 
